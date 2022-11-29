5f592ae367bcdc7e7d7e973467e22e95868b3fe6
Carlos Baptista <carlos.baptistaa@hotmail.com>
2022-11-29 11:14:52 -0300

    Condições If e Else

--- 01-fundamentos/section-04-introducao-a-javascript/if-else.js
+++ 01-fundamentos/section-04-introducao-a-javascript/if-else.js
@@ -1,6 +1,19 @@
 //if - se
+//senao se
 //else - senão
 
-if (condicao) {
-    //codigo
+let trybe = 19.40;
+
+
+if (trybe >= 14 && trybe < 14.40) {
+    console.log("Esquenta!");
 } 
+else if (trybe >= 16.30 && trybe < 17.50) {
+    console.log("Aula Ao Vivo!");}
+
+else if (trybe >= 19.40 && trybe < 20) {
+    console.log("Fechamento!");
+}
+else {
+    console.log("Fora dos momentos síncronos!");
+}

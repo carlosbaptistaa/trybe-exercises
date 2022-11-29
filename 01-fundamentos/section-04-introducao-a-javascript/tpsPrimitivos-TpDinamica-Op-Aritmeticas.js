b025613b41e74a04c72f1be3054f6ac01d7bec7e
Carlos Baptista <carlos.baptistaa@hotmail.com>
2022-11-29 10:48:03 -0300

    operadores aritmeticos e incrementos

--- 01-fundamentos/section-04-introducao-a-javascript/tpsPrimitivos-TpDinamica-Op-Aritmeticas.js
+++ 01-fundamentos/section-04-introducao-a-javascript/tpsPrimitivos-TpDinamica-Op-Aritmeticas.js
@@ -1,5 +1,22 @@
-let movie = 'Avengers'; //string literal
+/* let movie = 'Avengers'; //string literal
 let score = 10.90; //number literal
 let isValid = true; //boolean
 let name; //undefined
-let color = null; //redefinição
+let color = null; redefinição */
+
+//+, -, *, /, **
+
+let salary = 3500;
+
+/* console.log(salary + salary);
+console.log(salary - salary);
+console.log(salary * salary);
+console.log(salary / salary);
+console.log(3 ** 3);*/
+
+//Incrementar ++
+//Decrementar --
+
+salary--;
+
+console.log(salary); 

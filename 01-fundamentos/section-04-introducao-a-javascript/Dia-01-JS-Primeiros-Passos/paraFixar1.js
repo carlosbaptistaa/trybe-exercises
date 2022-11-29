16fdb886b680054145ee537950010bbb112e262f
Carlos Baptista <carlos.baptistaa@hotmail.com>
2022-11-29 10:02:06 -0300

    resolucao exercicio Para Fixar

---
+++ 01-fundamentos/section-04-introducao-a-javascript/Dia-01-JS-Primeiros-Passos/paraFixar1.js
@@ -0,0 +1,7 @@
+const myName ='Carlos Eduardo Duarte Baptista';
+const birthCity = 'Rio de Janeiro';
+// birthCity = 'Salvador';
+// não eh possível alterar o valor da const birthCity, pois eh uma constante e seu valor não muda.
+let birthYear = 2030;
+
+console.log(myName, birthCity, birthYear);
--- 01-fundamentos/section-04-introducao-a-javascript/Dia-01-JS-Primeiros-Passos/script.js
+++
@@ -1,5 +0,0 @@
-let fullName ='Carlos Eduardo';
-let age = 23;
-
-console.log(nome);
-console.log(age);

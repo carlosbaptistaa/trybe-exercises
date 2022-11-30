5601fb38b497b62d38141196c42c5afef1a90e69
Carlos Baptista <carlos.baptistaa@hotmail.com>
2022-11-30 10:51:00 -0300

    resolucao exercicio 6

---
+++ 01-fundamentos/section-04-introducao-a-javascript/exercicios-pratica/exercicio6.js
@@ -0,0 +1,24 @@
+//Utilize switch/case para escrever um código que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+
+let pecaXadrez = "Bispo".toLowerCase();
+
+switch (pecaXadrez) {
+
+    case "bispo":
+        console.log("Ande pela diagonal");
+        break;
+    case "peao":
+        console.log("Ande 1 para frente");
+        break;
+    
+    case "rainha":
+        console.log("Ande pela diagonal, frente, costas ou lados");
+        break;
+
+    case "torre":
+        console.log("Ande pela frente, costas ou lados ");
+        break;
+
+    default: 
+    console.log("Peça inválida!!!");
+}

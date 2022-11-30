c74899ee9e0e1bd52de47ba47e528808d9ec8cd9
Carlos Baptista <carlos.baptistaa@hotmail.com>
2022-11-30 11:49:56 -0300

    adicao bonus 1

---
+++ 01-fundamentos/section-04-introducao-a-javascript/exercicios-pratica/bonus1.js
@@ -0,0 +1,13 @@
+// Utilize if/else para escrever um código que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele deve retornar false. 
+// Bonus: use somente um if.
+
+const number1 = 8;
+const number2 = 1;
+const number3 = 5;
+
+
+if (number1 % 2 === 0 || number2 % 2 === 0 || number3  % 2 === 0){
+    console.log(true);
+} else {
+    console.log(false);
+}

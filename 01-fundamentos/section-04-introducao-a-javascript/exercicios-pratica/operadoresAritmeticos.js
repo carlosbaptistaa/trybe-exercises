4171a44a49ad9d25c1c164190d87928836dfc0ba
Carlos Baptista <carlos.baptistaa@hotmail.com>
2022-11-30 09:00:07 -0300

    resolucao exercicio de operadores aritmeticos

--- 01-fundamentos/section-04-introducao-a-javascript/exercicios-pratica/operadoresAritmeticos.js
+++ 01-fundamentos/section-04-introducao-a-javascript/exercicios-pratica/operadoresAritmeticos.js
@@ -1,17 +1,17 @@
-const number1 = 13;
+const number1 = 31;
 const number2 = 5;
 
 let adicao = number1 + number2;
-console.log("A adição dos números é:  ");
+console.log("A adição dos números é: " + adicao);
 
 let subtracao = number1 - number2;
-console.log("A subtração dos números é:  ");
+console.log("A subtração dos números é:  " + subtracao);
 
 let multiplicacao = number1 * number2;
-console.log("A multiplicação dos números é:  ");
+console.log("A multiplicação dos números é:  " + multiplicacao);
 
 let divisao = number1 / number2;
-console.log("A divisão dos números é:  ");
+console.log("A divisão dos números é:  " + divisao);
 
 let modulo = number1 % number2;
-console.log("O módulo dos números é:  ");
+console.log("O módulo dos números é:  " + modulo);

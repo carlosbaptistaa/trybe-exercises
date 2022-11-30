b11d03cc8ca9ccb309c493ae914d8ddcb6a71d6a
Carlos Baptista <103068255+carlosbaptistaa@users.noreply.github.com>
2022-11-30 08:55:43 -0300

    exercicio operadores arimeticos

---
+++ 01-fundamentos/section-04-introducao-a-javascript/exercicios-pratica/operadoresAritmeticos.js
@@ -0,0 +1,17 @@
+const number1 = 13;
+const number2 = 5;
+
+let adicao = number1 + number2;
+console.log("A adição dos números é:  ");
+
+let subtracao = number1 - number2;
+console.log("A subtração dos números é:  ");
+
+let multiplicacao = number1 * number2;
+console.log("A multiplicação dos números é:  ");
+
+let divisao = number1 / number2;
+console.log("A divisão dos números é:  ");
+
+let modulo = number1 % number2;
+console.log("O módulo dos números é:  ");

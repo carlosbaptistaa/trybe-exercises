2dc10e95aafeb2a2e9ed8b35fc197aa9dcc562dc
Carlos Baptista <carlos.baptistaa@hotmail.com>
2022-11-30 10:02:46 -0300

    resolucao exercicio else 2

--- 01-fundamentos/section-04-introducao-a-javascript/exercicios-pratica/elseif1.js
+++ 01-fundamentos/section-04-introducao-a-javascript/exercicios-pratica/elseif1.js
@@ -6,3 +6,17 @@ if (number1 > number2){
 } else {
     console.log("O maior número é: " + number2);
 }
+
+const number01 = 69;
+const number02 = 70;
+const number03 = 80;
+
+if (number01 > number02 && number01 > number03){
+    console.log("O maior número é: " + number01);
+}
+else if (number02 > number01 && number02 > number03){
+    console.log("O maior número é: " + number02);
+}
+else {
+    console.log("O maior número é: " + number03);
+}

fd5764b9a5ed0de06ffae95a4bf267e43a967bdc
Carlos Baptista <carlos.baptistaa@hotmail.com>
2022-12-01 09:36:50 -0300

    resolucao exercicio soma

--- 01-fundamentos/section-04-introducao-a-javascript/Dia-02-JS-Array-e-Loop-For/arraysListas.js
+++ 01-fundamentos/section-04-introducao-a-javascript/Dia-02-JS-Array-e-Loop-For/arraysListas.js
@@ -6,5 +6,5 @@ console.log(tasksList);
 //tasksList.pop(); remove a última tarefa
 //let searchForFirstTask = tasksList[0]; mostra o item de index 0
 
-let indexOfTask = tasksList.indexOf('Tomar café');
-console.log(indexOfTask);
+//let indexOfTask = tasksList.indexOf('Tomar café'); mostra o índice do item
+

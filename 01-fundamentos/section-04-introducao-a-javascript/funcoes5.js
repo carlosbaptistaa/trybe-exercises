1dee5580bd7cf5e7143d15778676a5e55cde5923
Carlos Baptista <carlos.baptistaa@hotmail.com>
2022-12-06 17:56:26 -0300

    resolucao do exercicio 5

--- 01-fundamentos/section-04-introducao-a-javascript/funcoes5.js
+++ 01-fundamentos/section-04-introducao-a-javascript/funcoes5.js
@@ -1,11 +1,16 @@
-function conferencia (num) {
-    if (num > 0) {
-        return 'O número é : ' + num + ' é POSITIVO';
+function angulosInternos (a, b, c) {
+    if (a + b + c === 180) {
+        return angulosInternos = true;
     }
-    else if (num < 0) {
-            return 'O número é : ' + num + ' é NEGATIVO';
-    } else {
-        return 'O número é nulo, pois é igual a ZERO!!!'
+    
+    else if (a < 0 || b < 0 || c < 0) {
+        return 'Um ângulo não possui valor negativo!!!'
+    }
+    else if (a + b + c > 0 && a + b + c < 180) {
+        return angulosInternos = false;
+
+    }else{
+        return 'Não é um triângulo retângulo!!'
     }
 }
-    console.log(conferencia(0));
+    console.log(angulosInternos(50, 50, 100));

2783ff63cae1f493ebb354a7df87b15de328e8a2
Carlos Baptista <carlos.baptistaa@hotmail.com>
2022-12-07 12:02:06 -0300

    exemplos de arrow function =>

---
+++ 01-fundamentos/section-04-introducao-a-javascript/Dia-05-JS-ES6-Let-Const-ArrowFunctions-E-TemplatesLiterals/Arrow-Functions/exemplo2.js
@@ -0,0 +1,10 @@
+// function contaPalavras (frase) {
+//     return frase.split(' ').length;
+// }
+
+// console.log(contaPalavras('Fala tribo, beleza?'));
+
+// ouuuuu
+
+const contaPalavras = frase => frase.split(' ').length 
+console.log(contaPalavras('Fala tribo, beleza?')); //3
---
+++ 01-fundamentos/section-04-introducao-a-javascript/Dia-05-JS-ES6-Let-Const-ArrowFunctions-E-TemplatesLiterals/Arrow-Functions/exemplo3.js
@@ -0,0 +1,13 @@
+// function objetoPessoa (nome, idade){
+//     return {
+//     nome: nome,
+//     idade: idade
+//     }
+// }
+
+// console.log(objetoPessoa('Cadu', 25));
+
+//ouuuuuuuu
+
+const objetoPessoa = (nome, idade) => ({nome: nome, idade: idade});
+console.log(objetoPessoa('Cadu', 25));

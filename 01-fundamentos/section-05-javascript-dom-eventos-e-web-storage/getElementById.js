598b85960344cb86b366b19afdcc30ee2374f6d9
Carlos Baptista <carlos.baptistaa@hotmail.com>
2022-12-08 12:02:51 -0300

    exercicio de DOM

---
+++ 01-fundamentos/section-05-javascript-dom-eventos-e-web-storage/getElementById.js
@@ -0,0 +1,5 @@
+document.getElementById("page-title").innerText = "Branca de Neve"
+
+document.getElementById("paragraph").innerText = "Este é o filme da princesa que encontra 7 anões na floresta."
+
+document.getElementById("subtitle").innerText = "e os 7 anões" 
---
+++ 01-fundamentos/section-05-javascript-dom-eventos-e-web-storage/getElementsByClassName.js
@@ -0,0 +1,6 @@
+let paragraphs = document.getElementsByClassName('paragraphs')
+
+for (let index = 0; index < paragraphs.length; index +=1) {
+    paragraphs[index].innerText = 'Novo Paragrafo'
+}
+
